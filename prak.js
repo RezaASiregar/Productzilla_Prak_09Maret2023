577cc343aa5d3bad528d5525ca0e4c8bcd2cf7ac
RezaASiregar <rezasiregar98@gmail.com>
2023-03-09 21:10:05 +0700

    Prak Task Object

---
+++ prak.js
@@ -0,0 +1,62 @@
+const musik = {
+    PopPunk: 'blink-182',
+    PopRock: 'BonJovi',
+    rock: 'Guns And Roses',
+    
+    band: function pop() {
+        console.log('SheilaOn7, Noah, Peterpan')
+    },
+
+    Kpop: ['Twice, RedVelvet, EXO, NCT'],
+
+    Jpop: 'AKB48',
+    
+    HC: hardcore = {
+        '1': 'BlackFlag',
+        '2': 'Metallica',
+        '3': 'Burgerkill',
+    },
+};
+
+const vespa = {
+    PXSeries: 'Indonesia',
+    PXESeries: 'Indent',
+    SpecialSeries: 'indent',
+
+    Piaggio: function jenis() {
+        console.log('PX, PTS, Kongo')
+    },
+
+    tipe2: ['Excel', 'Spartan'],
+
+    tipe3: 'Darling',
+
+    VS: tipe4 = {
+        '1': 'America Edition',
+        '2': 'Europe Edition',
+        '3': 'Asia Edition',
+    },
+};
+
+const laptop = {
+    low: 'aspire',
+    mid: 'ideapad',
+    high: 'legion',
+    
+    brand: function merk(){
+        console.log('LENOVO, ASUS, HP')
+    },
+
+    legionn: ['series1','series2','series3'],
+    ROG: 'Zhypirus',
+    
+    lp: Omen = {
+        seri1: 'omen1',
+        seri2: 'omen2',
+        seri3: 'omen3',
+    },
+};
+
+console.log(laptop);
+console.log(vespa);
+console.log(musik);
